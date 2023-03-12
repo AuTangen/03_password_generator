7f2f3a78e561a4be23640c21d31a568d873ee6ab
AuTangen <atangen2406@gmail.com>
2023-03-12 19:12:24 -0400

    save point, why arent these arrays concatinating

--- script.js
+++ script.js
@@ -3,84 +3,77 @@ var generateBtn = document.querySelector("#generate");
 
 var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-var special = ['!', '$', '&', '*', '#', '_']
-var chars = [];
-var chars2 = [];
-var chars3 = [];
-var chars4 = [];
-selectLength = [];
+var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = ['!', '$', '&', '*', '#', '_'];
+
+
+
 
 // var pass = [];
 // Write password to the #password input
+
 function getRandom(charSet) {
 return charSet[Math.floor(Math.random * charSet.length)];
 }
 
-function writePassword() {
-  var selectLength = Number(window.prompt("Enter password length: 8-128 characters",""));
-  if(selectLength >= 8 && selectLength <= 128) {
-    var typeLower = window.confirm("Would you like your password to contain lowercase letters?");
-    var typeUpper = window.confirm("Would you like your password to contain uppercase letters?");
-    var typeNumber = window.confirm("Would you like your password to contain numbers?");
-    var typeSpecial = window.confirm("Would you like your password to contain special characters?");
-    if (typeLower === true) {
-      chars = lower;
-      }
-      else {chars = []};
-      if (typeUpper === true) {
-      chars2 = chars.concat(upper);
-      }
-      else {chars2 = lower};
-      if (typeNumber === true) {
-      chars3 = chars2.concat(num);
-      }
-      else {chars3 = chars2};
-      if (typeSpecial === true) {
-      chars4 = chars3.concat(special);
-      }
-      else{chars4 = chars3};
-      // return chars4;
-      console.log(chars4)
-      console.log(selectLength);
-    }
+// function writePassword() {
+//   options.passwordLength = Number(window.prompt("Enter password length: 8-128 characters",""));
+//   if(options.passwordLength >= 8 && options.passwordLength <= 128) {
+//     options.lowercase = window.confirm("Would you like your password to contain lowercase letters?");
+//     options.uppercase = window.confirm("Would you like your password to contain uppercase letters?");
+//     options.numbers = window.confirm("Would you like your password to contain numbers?");
+//     options.special = window.confirm("Would you like your password to contain special characters?");
+//     console.log(options.passwordLength);
+//     console.log(options.lowercase);
+//     console.log(options.uppercase);
+//     console.log(options.numbers);
 
-    else {
-      alert("please enter a number between 8-128 TRY AGAIN")
+
+//   }
+
+  
+    
+
+//     else {
+//       alert("please enter a number between 8-128 TRY AGAIN")
       
-    }}
-
-     function generatePassword(options) {
-      var result = '';
-      for (var i = 0; i < selectLength; i++) {
-        result += getRandom(chars4);
-                     
-      }
-return result;
-
-      // let pass = ' ';
-      // const passlength = chars4.length;
-      // for (let i = 0; i < selectLength.length; i++) {
-      //   pass += chars4.charAt[Math.floor(Math.random()*passlength)];
-      //   return pass;
-      //             }
-      //   // return pass;
-      //   console.log(chars4);
-      //   console.log(selectLength);
-      //   console.log(typeLower);
-      //   console.log(lower);
-     
-    }
-    
-    
+//     }
+//   }
+var result = '';
+var chars = [];
+ function generatePassword(data) {
+ 
   
+  if (data.lowercase) {
+  chars = chars.concat(lower)
+  }
+  if (data.uppercase) {
+    chars = chars.concat(upper);
+  }
+  if (data.numbers) {
+    chars = chars.concat(num);
+  }
+  if (data.special) {
+    chars = chars.concat(special);
+  }
   
-  generatePassword();
-  var options = selectLength;
-  var password = generatePassword(chars4);
+  for (var i = 0; i < data.passwordLength; i++) {
+    result += getRandom(chars);
+  }
+  return result;
+  }
+
+  var options =  {
+    passwordLength: [8],
+    lowercase: [true],
+    uppercase: [true],
+    numbers: [true],
+    special: [true]
+  }
+  var password = generatePassword(options);
   var passwordText = document.querySelector("#password");
-  console.log(password);
   passwordText.value = password;
+  console.log(password);
  
   
 
@@ -90,9 +83,9 @@ return result;
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
 
 // ------------------------------
 // global arrays- lower, upper, num, special
 // parameters- selectLength, typeLower, typeUpper, typeNumber, typeSpecial
-
